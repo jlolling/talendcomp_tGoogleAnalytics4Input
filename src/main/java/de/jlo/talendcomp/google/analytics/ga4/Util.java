424d0614f3278d89a3f58145d6b49c59eb57fcac
Jan Lolling (Klöckner <jan.lolling@kloeckner.com>
2023-03-29 12:00:48 +0200

    Util.convertBoolean improved

--- src/main/java/de/jlo/talendcomp/google/analytics/ga4/Util.java
+++ src/main/java/de/jlo/talendcomp/google/analytics/ga4/Util.java
@@ -132,16 +132,50 @@ public class Util {
 	}
 
 	public static Boolean convertToBoolean(String value) throws Exception {
-		if (value == null || value.trim().isEmpty()) {
+		if (value == null) {
 			return null;
+		}
+		value = value.toLowerCase();
+		if ("true".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("false".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("1".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("0".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("yes".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("y".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("sí".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("да".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("no".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("нет".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("n".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("ja".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("j".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("nein".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("oui".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("non".equals(value)) {
+			return Boolean.FALSE;
+		} else if ("ok".equals(value)) {
+			return Boolean.TRUE;
+		} else if ("x".equals(value)) {
+			return Boolean.TRUE;
+		} else if (value != null) {
+			throw new Exception("Value: " + value + " cannot be parsed to a boolean");
 		} else {
-			if ("true".equals(value.trim())) {
-				return true;
-			} else if ("false".equals(value.trim())) {
-				return false;
-			} else {
-				throw new Exception("Value:" + value + " is not a boolean value!");
-			}
+			return null;
 		}
 	}
 
